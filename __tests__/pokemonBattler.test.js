b350b87eba10f70cf208c848242d66118edef730
Robert Anderson <robertanderson@Roberts-MacBook-Pro.local>
2022-07-08 12:00:26 +0100

    createTrainer has a belt of 6 balls

--- __tests__/pokemonBattler.test.js
+++ __tests__/pokemonBattler.test.js
@@ -1,4 +1,15 @@
-const {CreatePokemon, FireType, WaterType, GrassType, Charmander, Squirtle, Bulbasaur, Rattata} = require("../pokemonBattler");
+const {
+  CreatePokemon,
+  FireType,
+  WaterType,
+  GrassType,
+  Charmander,
+  Squirtle,
+  Bulbasaur,
+  Rattata,
+  CreatePokeball,
+  CreateTrainer,
+} = require("../pokemonBattler");
 
 // const Flareon = new CreatePokemon("Flareon", "fire", 65, 20, "fireblast");
 // const Vaporeon = new CreatePokemon("Vaporeon", "water", 70, 19, "hydropump");
@@ -7,25 +18,25 @@ const {CreatePokemon, FireType, WaterType, GrassType, Charmander, Squirtle, Bulb
 
 describe(CreatePokemon, () => {
   test("should return an object with the name property", () => {
-    const newPokemon = new CreatePokemon()
-    expect(newPokemon).toHaveProperty("name")
+    const newPokemon = new CreatePokemon();
+    expect(newPokemon).toHaveProperty("name");
   });
   test("should return an object with the type property", () => {
-    const newPokemon = new CreatePokemon()
-    expect(newPokemon).toHaveProperty("type", "normal")
-  })
+    const newPokemon = new CreatePokemon();
+    expect(newPokemon).toHaveProperty("type", "normal");
+  });
   test("should return an object with the hitPoints property", () => {
-    const newPokemon = new CreatePokemon()
-    expect(newPokemon).toHaveProperty("hitPoints")
-  })
+    const newPokemon = new CreatePokemon();
+    expect(newPokemon).toHaveProperty("hitPoints");
+  });
   test("should return an object with the attackDamage property", () => {
-    const newPokemon = new CreatePokemon()
-    expect(newPokemon).toHaveProperty("attackDamage")
-  })
+    const newPokemon = new CreatePokemon();
+    expect(newPokemon).toHaveProperty("attackDamage");
+  });
   test("should return an object with the move property", () => {
-    const newPokemon = new CreatePokemon()
-    expect(newPokemon).toHaveProperty("move")
-  })
+    const newPokemon = new CreatePokemon();
+    expect(newPokemon).toHaveProperty("move");
+  });
   test("isEffectiveAgainst should return true when water against fire/leaf is against water/fire is against leaf", () => {
     const Flareon = new FireType("Flareon", 65, 20, "fireblast");
     const Vaporeon = new WaterType("Vaporeon", 70, 19, "hydropump");
@@ -67,27 +78,126 @@ describe(CreatePokemon, () => {
 
   test("hasFainted should correct boolean when health is 0", () => {
     const Flareon = new FireType("Flareon", 65, 20, "fireblast");
-    Flareon.takeDamage(65)
-    expect(Flareon.hasFainted()).toEqual(true)
-  })
+    Flareon.takeDamage(65);
+    expect(Flareon.hasFainted()).toEqual(true);
+  });
 
   test("Charmander should be a FireType and have a move called ember", () => {
-    const testCharmander = new Charmander("Charmander", 68, 23, "flamethrower")
-    expect(testCharmander).toEqual({attackDamage: 23, hitPoints: 68, move: "ember", name: "Charmander", type: "fire"})
-  })
+    const testCharmander = new Charmander("Charmander", 68, 23, "flamethrower");
+    expect(testCharmander).toEqual({
+      attackDamage: 23,
+      hitPoints: 68,
+      move: "ember",
+      name: "Charmander",
+      type: "fire",
+    });
+  });
 
   test("Squirtle should be a WaterType and have a move called water gun", () => {
-    const testSquirtle = new Squirtle("Squirtle", 68, 23, "surf")
-    expect(testSquirtle).toEqual({attackDamage: 23, hitPoints: 68, move: "water gun", name: "Squirtle", type: "water"})
-  })
+    const testSquirtle = new Squirtle("Squirtle", 68, 23, "surf");
+    expect(testSquirtle).toEqual({
+      attackDamage: 23,
+      hitPoints: 68,
+      move: "water gun",
+      name: "Squirtle",
+      type: "water",
+    });
+  });
 
   test("Bulbasaur should be a GrassType and have a move called vine whip", () => {
-    const testBulbasaur = new Bulbasaur("Bulbasaur", 68, 23, "razor leaf")
-    expect(testBulbasaur).toEqual({attackDamage: 23, hitPoints: 68, move: "vine whip", name: "Bulbasaur", type: "grass"})
-  })
+    const testBulbasaur = new Bulbasaur("Bulbasaur", 68, 23, "razor leaf");
+    expect(testBulbasaur).toEqual({
+      attackDamage: 23,
+      hitPoints: 68,
+      move: "vine whip",
+      name: "Bulbasaur",
+      type: "grass",
+    });
+  });
 
   test("Rattata should be a Pokemon", () => {
-    const testRattata = new Rattata("Rattata", 50, 16)
-    expect(testRattata).toEqual({attackDamage: 16, hitPoints: 50, move: "tackle", name: "Rattata", type: "normal"})
-  })
+    const testRattata = new Rattata("Rattata", 50, 16);
+    expect(testRattata).toEqual({
+      attackDamage: 16,
+      hitPoints: 50,
+      move: "tackle",
+      name: "Rattata",
+      type: "normal",
+    });
+  });
+});
+
+describe(CreatePokeball, () => {
+  test("CreatePokeball should have a pokemon property", () => {
+    const newpokeball = new CreatePokeball();
+    expect(newpokeball).toHaveProperty("pokemon");
+  });
+  test("CreatePokeball should have a pokemon property, which defaults to empty", () => {
+    const newpokeball = new CreatePokeball();
+    expect(newpokeball).toHaveProperty("pokemon", "empty");
+  });
+  test("isEmpty should return true if the pokeball is empty", () => {
+    const testpokeball = new CreatePokeball();
+    expect(testpokeball.isEmpty()).toEqual(true);
+  });
+  test("isEmpty should return false if the pokeball contains a pokemon", () => {
+    const Flareon = new FireType("Flareon", 65, 20, "fireblast");
+    const testpokeball = new CreatePokeball(Flareon);
+    expect(testpokeball.isEmpty()).toEqual(false);
+  });
+  test("Throw should capture a passed pokemon if pokeball is empty", () => {
+    //Arrange
+    const Flareon = new FireType("Flareon", 65, 20, "fireblast");
+    const testpokeball = new CreatePokeball();
+    //Act
+    testpokeball.throw(Flareon);
+    //Assert
+    expect(testpokeball.pokemon).toEqual(Flareon);
+  });
+  test("Throw should not capture a passed pokemon if pokeball contains a pokemon", () => {
+    //Arrange
+    const Flareon = new FireType("Flareon", 65, 20, "fireblast");
+    const Vaporeon = new WaterType("Vaporeon", 70, 19, "hydropump");
+    const testpokeball = new CreatePokeball(Flareon);
+    //Act
+    testpokeball.throw(Vaporeon);
+    //Assert
+    expect(testpokeball.pokemon).toEqual(Flareon);
+  });
+  test("Throw should return the stored pokemon if invoked with no argument", () => {
+    //Arrange
+    const Flareon = new FireType("Flareon", 65, 20, "fireblast");
+    const testpokeball = new CreatePokeball(Flareon);
+    //Assert
+    expect(testpokeball.throw()).toEqual(Flareon);
+  });
+  test("Throw should return a message if pokeball is empty and invoked with no argument", () => {
+    //Arrange
+    const Flareon = new FireType("Flareon", 65, 20, "fireblast");
+    const testpokeball = new CreatePokeball();
+    //Assert
+    expect(testpokeball.throw()).toEqual("Pokeball is Empty!");
+  });
+  test("Contains should return empty if no pokemon is inside the pokeball", () => {
+    //Arrange
+    const testpokeball = new CreatePokeball();
+    //Assert
+    expect(testpokeball.contains()).toEqual("Empty...");
+  });
+  test("Contains should return pokemon name if pokeball contains a pokemon", () => {
+    //Arrange
+    const Flareon = new FireType("Flareon", 65, 20, "fireblast");
+    const testpokeball = new CreatePokeball(Flareon);
+    //Assert
+    expect(testpokeball.contains()).toEqual("Flareon");
+  });
+});
+
+describe(CreateTrainer, () => {
+  test("CreateTrainer should have multiple ball properties", () => {
+    const testTrainer = new CreateTrainer();
+    expect(testTrainer).toHaveProperty("ball1");
+    expect(testTrainer).toHaveProperty("ball4");
+    expect(testTrainer).toHaveProperty("ball6");
+  });
 });
--- pokemonBattler.js
+++ pokemonBattler.js
@@ -18,10 +18,10 @@ class CreatePokemon {
     }
     if (this.type === "normal") {
       return false;
-    } 
+    }
     return false;
   }
-  
+
   isWeakTo(rivalPokemon) {
     if (this.type === "fire" && rivalPokemon.type === "water") {
       return true;
@@ -36,82 +36,124 @@ class CreatePokemon {
       return false;
     } else return false;
   }
-  
+
   takeDamage(num) {
     this.hitPoints -= num;
   }
-  
+
   useMove() {
     console.log(`${this.name} used ${this.move}!`);
     return this.attackDamage;
   }
   hasFainted() {
-    return this.hitPoints === 0
+    return this.hitPoints === 0;
   }
 }
 
 class FireType extends CreatePokemon {
   constructor(name, hitPoints, attackDamage, move) {
-    super(name, hitPoints, attackDamage, move)
-    this.type = "fire"
+    super(name, hitPoints, attackDamage, move);
+    this.type = "fire";
   }
 }
 
 class WaterType extends CreatePokemon {
   constructor(name, hitPoints, attackDamage, move) {
-    super(name, hitPoints, attackDamage, move)
-    this.type = "water"
+    super(name, hitPoints, attackDamage, move);
+    this.type = "water";
   }
 }
 
 class GrassType extends CreatePokemon {
   constructor(name, hitPoints, attackDamage, move) {
-    super(name, hitPoints, attackDamage, move)
-    this.type = "grass"
+    super(name, hitPoints, attackDamage, move);
+    this.type = "grass";
   }
 }
 
 class Charmander extends FireType {
   constructor(name, hitPoints, attackDamage, move) {
-    super(name, hitPoints, attackDamage, move)
-    this.name = "Charmander"
-    this.move = "ember"
+    super(name, hitPoints, attackDamage, move);
+    this.name = "Charmander";
+    this.move = "ember";
   }
 }
 
 class Squirtle extends WaterType {
   constructor(name, hitPoints, attackDamage, move) {
-    super(name, hitPoints, attackDamage, move)
-    this.name = "Squirtle"
-    this.move = "water gun"
+    super(name, hitPoints, attackDamage, move);
+    this.name = "Squirtle";
+    this.move = "water gun";
   }
 }
 
 class Bulbasaur extends GrassType {
   constructor(name, hitPoints, attackDamage, move) {
-    super(name, hitPoints, attackDamage, move)
-    this.name = "Bulbasaur"
-    this.move = "vine whip"
+    super(name, hitPoints, attackDamage, move);
+    this.name = "Bulbasaur";
+    this.move = "vine whip";
   }
 }
 
 class Rattata extends CreatePokemon {
   constructor(name, hitPoints, attackDamage, move) {
-    super(name, hitPoints, attackDamage, move)
-    this.name = "Rattata"
+    super(name, hitPoints, attackDamage, move);
+    this.name = "Rattata";
   }
 }
 
+// const Eevee = new CreatePokemon("Eevee", "normal", 55, 18, "headbutt");
+// const Flareon = new CreatePokemon("Flareon", "fire", 65, 20, "fireblast");
+// const Vaporeon = new CreatePokemon("Vaporeon", "water", 70, 19, "hydropump");
+// const Leafeon = new CreatePokemon("Leafeon", "grass", 65, 17, "gigadrain");
 
-const Eevee = new CreatePokemon("Eevee", "normal", 55, 18, "headbutt");
-const Flareon = new CreatePokemon("Flareon", "fire", 65, 20, "fireblast");
-const Vaporeon = new CreatePokemon("Vaporeon", "water", 70, 19, "hydropump");
-const Leafeon = new CreatePokemon("Leafeon", "grass", 65, 17, "gigadrain");
-// const Charmander = new CreatePokemon("Charmander", "fire", 44, 17, "flame thrower")
-// const Squirtle = new CreatePokemon("Squirtle", "water", 44, 16, "surf")
-// const Bulbasaur = new CreatePokemon ("Bulbasaur", "grass", 45, 16, "razor leaf")
-//const Charmander = new FireType("Charmander", 44, 16, "ember")
+class CreatePokeball {
+  constructor(pokemon = "empty") {
+    this.pokemon = pokemon;
+  }
+  isEmpty() {
+    if (this.pokemon === "empty") {
+      return true;
+    } else return false;
+  }
+  throw(pokemon) {
+    if (pokemon === undefined) {
+      if (this.pokemon === "empty") {
+        return "Pokeball is Empty!";
+      } else console.log(`Go ${this.pokemon.name}!`);
+      return this.pokemon;
+    }
+    if (this.pokemon === "empty") {
+      this.pokemon = pokemon;
+      console.log(`You caught ${pokemon.name}!`);
+    } else console.log(`Pokeball already has a pokemon inside!`);
+  }
+  contains() {
+    if (this.pokemon === "empty") {
+      return "Empty...";
+    } else return this.pokemon.name;
+  }
+}
+class CreateTrainer {
+  constructor(ball1, ball2, ball3, ball4, ball5, ball6) {
+    this.ball1 = ball1;
+    this.ball2 = ball2;
+    this.ball3 = ball3;
+    this.ball4 = ball4;
+    this.ball5 = ball5;
+    this.ball6 = ball6;
+  }
+}
 
-//console.log(Charmander)
-
-module.exports = {CreatePokemon, FireType, WaterType, GrassType, Charmander, Squirtle, Bulbasaur, Rattata};
+module.exports = {
+  CreatePokemon,
+  FireType,
+  WaterType,
+  GrassType,
+  Charmander,
+  Squirtle,
+  Bulbasaur,
+  Rattata,
+  CreatePokeball,
+  CreateTrainer,
+};
